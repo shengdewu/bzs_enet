39170c63ec2267d7daf6b4cca51f034787b3d926
shengdewu <786222104@qq.com>
2020-05-28 09:15:32 +0800

    complete the prediction model

---
+++ lanenet_infer.py
@@ -0,0 +1,12 @@
+from lannet.lanenet_predict import lanenet_predict
+from config.config import config
+from log.log_configure import log_configure
+import sys
+
+if __name__ == '__main__':
+    lannet_model = lanenet_predict()
+    network_config = config.get_config(sys.argv[1])
+    log_configure.init_log('lanenet_infer', network_config['log_path'])
+
+    lannet_model.infer(network_config)
+    print('infer success!!')
