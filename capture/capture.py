417d75774dfe7b19f7eee4a115ff1bd2a56f2340
baoyong <baoyong@klicen.com>
2020-08-21 09:47:09 +0800

    update

--- capture/capture.py
+++ capture/capture.py
@@ -7,26 +7,26 @@ class capture():
         :param place: 0 内置； 1 外置
         '''
         self._display = display
-        # self._cap = cv2.VideoCapture(place)
-        # if not self._cap.isOpened():
-        #     raise RuntimeError("can't open camera (0 itself; 1 usb) {}".format(place))
+        self._cap = cv2.VideoCapture(place)
+        if not self._cap.isOpened():
+            raise RuntimeError("can't open camera (0 itself; 1 usb) {}".format(place))
         return
 
-    # def capture(self):
-    #     ret, frame = self._cap.read()
-    #     if ret:
-    #         if self._display:
-    #             cv2.imshow('capture', frame)
-    #             cv2.waitKey(1)
-    #         return frame
-    #     return None
+    def capture(self):
+        ret, frame = self._cap.read()
+        if ret:
+            if self._display:
+                cv2.imshow('capture', frame)
+                cv2.waitKey(1)
+            return frame
+        return None
 
-    def test(self):
-        frame = cv2.imread('D:/work_space/tuSimpleDataSetSource/test\clips/0530/1492626047222176976_0/1.jpg', cv2.IMREAD_COLOR)
-        if self._display:
-            cv2.imshow('capture', frame)
-            cv2.waitKey(1)
-        return frame
+    # def test(self):
+    #     frame = cv2.imread('D:/work_space/tuSimpleDataSetSource/test/20/3-20.jpg', cv2.IMREAD_COLOR)
+    #     if self._display:
+    #         cv2.imshow('capture', frame)
+    #         cv2.waitKey(30)
+    #     return frame
 
 
 
