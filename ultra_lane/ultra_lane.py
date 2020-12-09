9a4c87bede115b22522e141bcdfed7b94ae144ff
baoyong <baoyong@klicen.com>
2020-12-09 20:29:14 +0800

    udpate

--- ultra_lane/ultra_lane.py
+++ ultra_lane/ultra_lane.py
@@ -5,6 +5,8 @@ import util.data_pipe
 from ultra_lane.data_stream import data_stream
 import tusimple_process.ultranet_comm
 import cv2
+from ultra_lane.similarity_loss import similaryit_loss
+from ultra_lane.similarity_loss import structural_loss
 
 class ultra_lane():
     def __init__(self):
@@ -14,10 +16,8 @@ class ultra_lane():
         self._lanes = tusimple_process.ultranet_comm.LANES
         return
 
-    def make_net(self, x, label, width, height, trainable=True, reuse=False):
+    def make_net(self, x, label, trainable=True, reuse=False):
         b, w, h, c = label.get_shape().as_list()
-        #x.set_shape(shape=[b, width, height, c])
-        #label.set_shape(shape=(b, len(self._row_anchors), self._lanes, 1))
 
         resnet_model = resnet()
         resnet_model.resnet18(x, self._cells+1, trainable, reuse)
@@ -35,7 +35,12 @@ class ultra_lane():
         label = tf.reshape(label, (b, w, h))
         label_oh = tf.one_hot(label, self._cells+1)
         cls = tf.losses.softmax_cross_entropy(label_oh, group_cls)
-        return cls
+
+        sim = similaryit_loss(group_cls)
+
+        shp = structural_loss(group_cls)
+
+        return cls, sim, shp
 
     def train(self, config):
         data_handle = data_stream(config['image_path'], config['img_width'], config['img_height'])
@@ -43,15 +48,11 @@ class ultra_lane():
         with tf.device(config['device']):
             src_tensor, cls_tensor = data_handle.create_img_tensor()
             src_img_queue, cls_label_queue = pipe_handle.make_pipe(config['batch_size'], (src_tensor, cls_tensor), data_handle.pre_process_img)
-            self.make_net(src_img_queue, cls_label_queue, config['img_width'], config['img_height'])
-
+            cls_loss_tensor, sim_loss_tensor, shp_loss_tensor = self.make_net(src_img_queue, cls_label_queue)
+            total_loss_tensor = cls_loss_tensor + sim_loss_tensor + shp_loss_tensor
             with tf.Session(config=tf.ConfigProto(log_device_placement=config['device_log'])) as sess:
                 sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
-                src_img, cls_label = sess.run([src_img_queue, cls_label_queue])
-                cv2.imshow('src_img', src_img[0])
-                cv2.imshow('cls', cls_label[0])
-                cv2.waitKey()
-                cv2.destroyAllWindows()
-                print('')
+                total_loss = sess.run(total_loss_tensor)
+                print(total_loss)
         return
 
