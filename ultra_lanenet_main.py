635dc92be79c43e0987faa111106765386705743
baoyong <baoyong@klicen.com>
2020-12-23 14:36:48 +0800

    update

--- ultra_lanenet_main.py
+++ ultra_lanenet_main.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     # net = ultra_model.make_net(x, 100, 56, 4)
 
     network_config = config.get_config(sys.argv[1])
-    log_configure.init_log('ultra_train', network_config['log_path'])
+    log_configure.init_log('ultra_train', network_config['result_path']+'/log')
 
     ultra_model.train(network_config)
     print('train success!!')
